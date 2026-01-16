b1306cd94969d810391a31f01b4312e63f81989e
Ekaterina Ostreva <lyapka_07@mail.ru>
2026-01-16 19:58:09 +0300

    ScriptChanges

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -7,10 +7,27 @@ public class Mine : MonoBehaviour
     [SerializeField] private AgentCharacter _character;
     [SerializeField] private ParticleSystem _explosionEffectPrefab;
 
+    private float _timer;
+    private float _timerDuration = 2f;
+    private bool _isCountingDown;
+
+    private void Awake()
+    {
+        _timer = _timerDuration;
+    }
+
     private void Update()
     {
-        if(IsInExplosionArea())
-            Explode();
+        if (IsInExplosionArea())
+            _isCountingDown = true; 
+
+        if(_isCountingDown == true)
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0)
+                Explode();
+        }
     }
 
     private void OnDrawGizmos()
@@ -21,7 +38,9 @@ public class Mine : MonoBehaviour
 
     public void Explode()
     {
-        _character.TakeDamage(_damage);
+        if (IsInExplosionArea())
+            _character.TakeDamage(_damage);
+
         ParticleSystem explosionEffect = Object.Instantiate(_explosionEffectPrefab, transform.position, Quaternion.identity);
         explosionEffect.Play();
 
@@ -31,6 +50,6 @@ public class Mine : MonoBehaviour
         Object.Destroy(explosionEffect.gameObject, duration);
     }
 
-    
     private bool IsInExplosionArea() => (_character.transform.position - transform.position).magnitude <= _radius;
+    
 }
